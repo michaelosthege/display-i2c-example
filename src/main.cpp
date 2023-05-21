d9cd7cc56d50fe33ae3d91e1b2cd82fa8f9ce64f
Michael Osthege <michael.osthege@outlook.com>
2023-05-21 21:18:49 +0200

    Add code

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <U8g2lib.h>
+#include <hornet_fonts.h>
+
+// The constructor below determines the type of display,
+// and the way how it's connected.
+// For more options and documentation see https://github.com/olikraus/u8g2/wiki/u8g2setupcpp
+// Another useful resource is this video: https://www.youtube.com/watch?v=MHogSbRPa28
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C display(U8G2_R0, PIN_A4, PIN_A5);
+
+// For cycling fonts:
+uint8_t f = 0;
+const uint8_t* fonts[] {
+    u8g2_DcsFont_tf,  // looks like a 16-segment display
+    u8g2_DcsFontHornet2_BIOS_09_tf,  // for the scratch display
+    u8g2_DcsFontHornet3_BIOS_09_tf,  // for the option displays
+    u8g2_DcsFontHornet4_BIOS_09_tf,  // for the scratch display
+};
+
+void setup()
+{
+    Serial.begin(9600);
+    display.begin();
+}
+
+void loop()
+{
+    // Cycle through fonts
+    Serial.println(f);
+    display.setFont(fonts[f]);
+    f++;
+    if (f == sizeof(fonts) / sizeof(fonts[0]))
+    {
+        f = 0;
+    }
+
+    // Write something
+    display.clearBuffer();
+    display.drawStr(0, 32, "|ABC123");
+    display.sendBuffer();
+
+    delay(1000);
+}
